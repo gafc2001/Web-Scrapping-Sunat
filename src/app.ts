e7f5582d37c8583ba844cbd8232e6e28262c7c31
Gustavo Farfan <farfancoraje15@gmail.com>
2024-06-18 11:40:06 -0500

    RucResult model

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express, { Request, Response } from 'express';
+import { RucResult } from './models/RucResult';
+import { CustomResponse } from './models/Response';
 const app = express();
 const port = 3000;
 
-app.get("/", (req : Request, res : Response) => {
-  res.json({
+app.get("/", (req : Request, res : CustomResponse<RucResult>) => {
+
+  const tipoBusqueda : string | undefined = req.get("tipo_busqueda");
+  const tipoDocumento : string | undefined = req.get("tipoDocumento");
+  const value : string | undefined = req.get("value");
+
+  
+  const result : RucResult ={
     ruc: "mobile",
-    tipo_contribuyente: 30,
+    tipo_contribuyente: "SOCIEDAD ANONIMA CERRADA",
     nombre_comercial: "Soul tesla Northeast",
-    fecha_inscripcion: 1718725296,
+    fecha_inscripcion: new Date(),
     estado_contribuyente: "Specialist",
     condicion_contribuyente: "woot",
     domicilio_fiscal: "375",
@@ -15,14 +23,15 @@ app.get("/", (req : Request, res : Response) => {
     sistema_contabilidad: "Convertible",
     actividades_economicas: [],
     comprantes_pago: [],
-    sistema_emision_electronica: "Norwegian",
-    emisor_electronico_desde: 1718725296,
+    sistema_emision_electronica: [],
+    emisor_electronico_desde: new Date(),
     comprobantes_electronicos: "turquoise",
-    afiliado_ple_desde: 1718725296,
+    afiliado_ple_desde: new Date(),
     padrones: "black",
-    fecha_inicio_actividades: 1718725296,
-    id: "1",
-  });
+    fecha_inicio_actividades: new Date(),
+  }
+
+  res.json(result);
 });
 
 app.listen(port, () => {
