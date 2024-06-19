a2618c1f04c0f5461fc18634b57986adcf480e79
Julian Quispe <julianmql@hotmail.com>
2024-06-19 07:21:40 -0500

    feat: ruc search endpoint added & stuff improved

--- src/utils/rucBusqueda.ts
+++ src/utils/rucBusqueda.ts
@@ -1,70 +1,102 @@
-import { RazonSocial } from "../models/RazonSocial";
+import { RucResult } from "../models/RucResult";
 
 const puppeteer = require('puppeteer');
 
-export const busquedaCoincidencias = async (razonSocial : string) : Promise<RazonSocial[]>=> {
-  let coincidencias = [];
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
-  const page = await browser.newPage();
+const {Browser,Page} = puppeteer;
 
+export const rucBusqueda = async (ruc: string): Promise<RucResult> => {
+  let browser: typeof Browser | null = null;
   
-  try{
-
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
-  
-    await page.goto('https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp', { waitUntil: 'networkidle2' });
-  
-    await page.waitForSelector('#btnPorRazonSocial');
-  
-    await page.click('#btnPorRazonSocial');
-  
-    await page.waitForSelector('#txtNombreRazonSocial');
-    await page.type('#txtNombreRazonSocial', razonSocial);
-  
-    await page.waitForSelector('#txtNombreRazonSocial');
-    await page.click('#btnAceptar');
-  
-  
-    await page.waitForSelector(".list-group-item.clearfix.aRucs[data-ruc]");
-  
-    await page.content();
-  
-    coincidencias = await page.evaluate(() => {
-      const resultadoElement = document.querySelectorAll(".list-group-item.clearfix.aRucs[data-ruc]");
-      let results : any[] = [];
-      resultadoElement.forEach( (el)  => {
-        results.push({
-          ruc : el.querySelectorAll<HTMLElement>(".list-group-item-heading")[0].innerText.split(":")[1].trim(),
-          razonSocial : el.querySelectorAll<HTMLElement>(".list-group-item-heading")[1].innerText,
-          ubicacion : el.querySelectorAll<HTMLElement>(".list-group-item-text")[0].innerText,
-          estado : el.querySelector<HTMLElement>(".list-group-item-text strong")?.innerText,
-        })
-      })
-      return results;
-    });
-  }catch(err){
-    console.log(err);
-  }
-  await browser.close();
-  return coincidencias;
-}
-export const coincidenciasRazonSocial = async (ruc : string) => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
   try {
-    const page = await browser.newPage();
-    
-    await page.waitForSelector(`a[data-ruc='${ruc}']`);
-    await page.click(`a[data-ruc='${ruc}']`);
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
 
-    await browser.close();
+    const page: typeof Page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    await page.goto('https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp', { waitUntil: 'networkidle2' });
+
+    await page.waitForSelector('#txtRuc');
+    await page.type('#txtRuc', ruc);
+
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: 'networkidle2' }),
+      page.click('#btnAceptar'),
+    ]);
+
+    await page.waitForSelector('.col-sm-7 .list-group-item-heading');
+
+    var parserDatevar = function(str: string): Date {
+      const [day, month, year] = str.split('/').map(Number);
+      return new Date(year, month - 1, day);
+    }
+    await page.exposeFunction("parserDatevar", parserDatevar);
+
+    // extraer la información, se almacena los valores de campos en array
+    const resultArray = await page.evaluate(() => {
+      const items = document.querySelectorAll('.list-group-item');
+      const resultArray: string[] = [];
+
+      Array.from(items).forEach(item => {
+        const columns = item.querySelectorAll('.col-sm-5, .col-sm-7, .col-sm-3');
+        // se toma de dos en dos los datos datos(key, value)
+        if (columns.length % 2 === 0) { 
+          for (let i = 0; i < columns.length; i += 2) {
+            let value = columns[i + 1].querySelector('h4, p')?.textContent?.trim() || '';
+
+            const table = columns[i + 1].querySelector('table');
+            if (table) {
+              const tdValues = Array.from(table.querySelectorAll('tr td')).map(td => td.textContent?.trim() || '');
+              value = tdValues.join(', ');
+            }
+
+            // se remueve los espacios o caracteres extra
+            value = value.replace(/\s+/g, ' ').trim();
+
+            resultArray.push(value);
+          }
+        }
+      });
+
+      return resultArray;
+    });
+
+    // funcion para convertir fecha de Sunat a Date
+    const parseDate = (str: string): Date => {
+      const [day, month, year] = str.split('/').map(Number);
+      return new Date(year, month - 1, day);
+    }
+
+    const rucResult: RucResult = {
+      ruc: resultArray[0] || '',
+      tipo_contribuyente: resultArray[1] || '',
+      tipo_documento: resultArray[2] || '',
+      nombre_comercial: resultArray[3] || '',
+      fecha_inscripcion: parseDate(resultArray[4] || ''),
+      fecha_inicio_actividades: parseDate(resultArray[5] || ''),
+      estado_contribuyente: resultArray[6] || '',
+      condicion_contribuyente: resultArray[7] || '',
+      domicilio_fiscal: resultArray[8] || '',
+      sistema_emision_comprobante: resultArray[9] || '',
+      actividad_comercio_exterior: resultArray[10] || '',
+      sistema_contabilidad: resultArray[11] || '',
+      actividades_economicas: (resultArray[12] || '').split(', '),
+      comprobantes_pago: (resultArray[13] || '').split(', '),
+      sistema_emision_electronica: (resultArray[14] || '').split(', '),
+      emisor_electronico_desde: parseDate(resultArray[15] || ''),
+      comprobantes_electronicos: resultArray[16] || '',
+      afiliado_ple_desde: resultArray[17] ? parseDate(resultArray[17]) : undefined,
+      padrones: (resultArray[18] || '').split(', '),
+    };
+
+    return rucResult;;
   } catch (error) {
-    console.error('Error en el script:', error);
-    await browser.close();
+    console.error('Error en rucBusqueda:', error);
+    throw new Error('Error en la consulta');
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
-};
+}
